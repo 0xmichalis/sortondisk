af76df2203f9e2d79b0f5d1852b4649d23d807e7
Michalis Kargakis <kargakis@protonmail.com>
2021-10-04 10:21:02 +0200

    Unit test update

--- pkg/sort/sorter_test.go
+++ pkg/sort/sorter_test.go
@@ -3,7 +3,6 @@ package sort_test
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -11,19 +10,24 @@ import (
 	"github.com/kargakis/sorter/pkg/sort"
 )
 
-func getLinesForFile(file *os.File) ([]*sort.Line, error) {
-	content := make([]*sort.Line, 0)
+func getLinesForFile(t *testing.T, path string) []*sort.Line {
+	file, err := os.Open(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
 
+	content := make([]*sort.Line, 0)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		var line sort.Line
 		if err := json.Unmarshal(scanner.Bytes(), &line); err != nil {
-			return nil, fmt.Errorf("Failed to unmarshal %s: %v\n", scanner.Text(), err)
+			t.Fatalf("Failed to unmarshal %s: %v\n", scanner.Text(), err)
 		}
 		content = append(content, &line)
 	}
 
-	return content, nil
+	return content
 }
 
 func sliceToMap(lines []*sort.Line) map[string]string {
@@ -35,25 +39,12 @@ func sliceToMap(lines []*sort.Line) map[string]string {
 }
 
 func TestSortByAddress(t *testing.T) {
-	expectedOut, err := os.Open("../../test/data_address")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer expectedOut.Close()
-	expectedOutContent, err := getLinesForFile(expectedOut)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	// Prepare inputs
 	input, err := os.Open("../../test/data.in")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer input.Close()
-	inputContent, err := getLinesForFile(input)
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	gotOut, err := ioutil.TempFile("", "sort_by_address_test")
 	if err != nil {
@@ -61,17 +52,18 @@ func TestSortByAddress(t *testing.T) {
 	}
 	defer os.Remove(gotOut.Name())
 
+	// Run sorter
 	s := sort.New(25, true, false, gotOut.Name())
 	if err := s.Sort(input); err != nil {
 		t.Fatal(err)
 	}
-	gotOutContent, err := getLinesForFile(gotOut)
-	if err != nil {
-		t.Fatal(err)
-	}
+
+	// Compare outputs
+	expectedOutContent := getLinesForFile(t, "../../test/data_address")
+	gotOutContent := getLinesForFile(t, gotOut.Name())
 
 	if len(gotOutContent) != len(expectedOutContent) {
-		t.Fatalf("expected output file length %d, got %d", len(expectedOutContent), len(gotOutContent))
+		t.Fatalf("expected output file length %d, got %d (%s)", len(expectedOutContent), len(gotOutContent), gotOut.Name())
 	}
 
 	// Compare expectedOut with gotOut content
@@ -86,6 +78,7 @@ func TestSortByAddress(t *testing.T) {
 	}
 
 	// Ensure input content matches output content in loc and values
+	inputContent := getLinesForFile(t, "../../test/data.in")
 	gotOutMap := sliceToMap(gotOutContent)
 	for name, address := range sliceToMap(inputContent) {
 		gotAddress, ok := gotOutMap[name]
@@ -99,12 +92,7 @@ func TestSortByAddress(t *testing.T) {
 }
 
 func TestSortByName(t *testing.T) {
-	expectedOut, err := os.Open("../../test/data_address")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer expectedOut.Close()
-
+	// Prepare inputs
 	input, err := os.Open("../../test/data.in")
 	if err != nil {
 		t.Fatal(err)
@@ -117,11 +105,41 @@ func TestSortByName(t *testing.T) {
 	}
 	defer os.Remove(gotOut.Name())
 
-	s := sort.New(25, false, true, gotOut.Name())
+	// Run sorter
+	s := sort.New(25, true, false, gotOut.Name())
 	if err := s.Sort(input); err != nil {
 		t.Fatal(err)
 	}
 
-	// TODO: Compare expectedOut with gotOut content
-	// TODO: Ensure input loc matches output loc
+	// Compare outputs
+	expectedOutContent := getLinesForFile(t, "../../test/data_name")
+	gotOutContent := getLinesForFile(t, gotOut.Name())
+
+	if len(gotOutContent) != len(expectedOutContent) {
+		t.Fatalf("expected output file length %d, got %d (%s)", len(expectedOutContent), len(gotOutContent), gotOut.Name())
+	}
+
+	// Compare expectedOut with gotOut content
+	for i, line := range expectedOutContent {
+		gotLine := gotOutContent[i]
+		if line.Name != gotLine.Name {
+			t.Fatalf("expected name %s, got %s", line.Name, gotLine.Name)
+		}
+		if line.Address != gotLine.Address {
+			t.Fatalf("expected address %s, got %s", line.Address, gotLine.Address)
+		}
+	}
+
+	// Ensure input content matches output content in loc and values
+	inputContent := getLinesForFile(t, "../../test/data.in")
+	gotOutMap := sliceToMap(gotOutContent)
+	for name, address := range sliceToMap(inputContent) {
+		gotAddress, ok := gotOutMap[name]
+		if !ok {
+			t.Fatalf("missing name %s in original input", name)
+		}
+		if address != gotAddress {
+			t.Fatalf("expected address %s, got %s", address, gotAddress)
+		}
+	}
 }
